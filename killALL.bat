aed7cec685fe226dc97d81c45c098eab5b742f07
Alessio <alessio.maiola@yahoo.it>
2024-03-19 02:09:08 +0100

    fix Jenkinsfile-windows

--- killALL.bat
+++ killALL.bat
@@ -4,3 +4,4 @@ kubectl delete -f .\login\service\emofy-db-container-service.yaml
 kubectl delete -f .\login\deployment\emofy-login-service-deployment.yaml
 kubectl delete -f .\login\service\emofy-login-service-service.yaml
 kubectl delete -f .\login\deployment\emofy-db-container-claim0-persistentvolumeclaim.yaml
+minikube delete
