12d63c4963af0d8f4c3a15b3212b572d6cb9ccae
Placido Pellegriti <placidopellegriti@live.it>
2024-07-21 12:02:16 +0200

    Implemented face detection and tests

---
+++ emotion_classification/face_detector.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+import base64
+
+class FaceDetector:
+
+    def __init__(self):
+        self.face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+    @staticmethod
+    def decode_image(base64_str):
+        img_data = base64.b64decode(base64_str)
+        np_arr = np.frombuffer(img_data, np.uint8)
+        img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+        return img
+
+    def detect_face(self, base64_img):
+        img = self.decode_image(base64_img)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = self.face_cascade.detectMultiScale(gray, 1.3, 5)
+        return len(faces) > 0
+
+    
---
+++ emotion_classification/test.py
@@ -0,0 +1,20 @@
+from server import app
+import cv2
+import base64
+
+if __name__ == '__main__':
+    with app.test_client() as client:
+        img = cv2.imread('test_images/minivan.jpg')
+        _, buffer = cv2.imencode('.jpg', img)
+        img_str = base64.b64encode(buffer).decode('utf-8')
+        response = client.post('/detect_face', json={'image': img_str})
+        assert response.status_code == 200
+        assert response.json['face_detected'] == False
+
+        img = cv2.imread('test_images/happy.jpg')
+        _, buffer = cv2.imencode('.jpg', img)
+        img_str = base64.b64encode(buffer).decode('utf-8')
+        response = client.post('/detect_face', json={'image': img_str})
+        assert response.status_code == 200
+        assert response.json['face_detected'] == True
+
