150471cc8a35eacfdb2d679e56a2990d8029cfe3
Placido Pellegriti <placidopellegriti@live.it>
2024-07-28 13:00:24 +0200

    Script addestramento iniziale

---
+++ emotion_classification/serving/training.py
@@ -0,0 +1,73 @@
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import PIL
+
+# Set the paths to the dataset
+train_dir = 'fer-2013/train'
+test_dir = 'fer-2013/test'
+
+# Create an ImageDataGenerator without data augmentation
+train_datagen = ImageDataGenerator(rescale=1.0/255)
+
+test_datagen = ImageDataGenerator(rescale=1.0/255)
+
+
+# Create generators to read images from the directories
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(48, 48),
+    color_mode='grayscale',
+    batch_size=64,
+    class_mode='categorical'
+)
+
+test_generator = test_datagen.flow_from_directory(
+    test_dir,
+    target_size=(48, 48),
+    color_mode='grayscale',
+    batch_size=64,
+    class_mode='categorical'
+)
+
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+
+model = Sequential()
+
+# Add convolutional layers
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(48, 48, 1)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+# Add fully connected layers
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(7, activation='softmax'))
+
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+
+history = model.fit(
+    train_generator,
+    steps_per_epoch=train_generator.samples // train_generator.batch_size,
+    epochs=30,
+    validation_data=test_generator,
+    validation_steps=test_generator.samples // test_generator.batch_size
+)
+
+
+test_loss, test_accuracy = model.evaluate(test_generator)
+print(f'Test Accuracy: {test_accuracy}')
+
+
+model.save('emotion_recognition_model.h5')
