2c85271953a798a52c44d44d3ff7a858cb1392c2
Placido Pellegriti <placidopellegriti@live.it>
2024-08-05 00:00:36 +0200

    Aggiunto parametri di avvio e addon necessari per supporto GPU

--- emotion_classification/serving/script/createall.bat
+++ emotion_classification/serving/script/createall.bat
@@ -1,6 +1,10 @@
 call emotion_classification\serving\script\updatedockerimage.bat
 
 kubectl apply -f https://raw.githubusercontent.com/NVIDIA/k8s-device-plugin/v0.12.3/nvidia-device-plugin.yml
+minikube addons enable nvidia-driver-installer
+minikube addons enable nvidia-gpu-device-plugin
+minikube addons enable nvidia-device-plugin
+
 
 kubectl create -f ./emotion_classification/serving/k8/service-reader-role.yaml
 kubectl create -f ./emotion_classification/serving/k8/service-reader-rolebinding.yaml
