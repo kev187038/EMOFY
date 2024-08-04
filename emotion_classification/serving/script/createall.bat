50c198daddb893af2c1e9bcc02e218220b39fc19
Placido Pellegriti <placidopellegriti@live.it>
2024-08-04 18:04:07 +0200

    Corretto path per aggiornamento immagine

--- emotion_classification/serving/script/createall.bat
+++ emotion_classification/serving/script/createall.bat
@@ -1,4 +1,4 @@
-call emotion_classification\script\updatedockerimage.bat
+call emotion_classification\serving\script\updatedockerimage.bat
 
 kubectl apply -f https://raw.githubusercontent.com/NVIDIA/k8s-device-plugin/v0.12.3/nvidia-device-plugin.yml
 
