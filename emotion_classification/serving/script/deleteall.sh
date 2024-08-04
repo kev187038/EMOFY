f32c0d5fe71f4fa8ce32877685076ccc5bd4ef45
Placido Pellegriti <placidopellegriti@live.it>
2024-08-04 18:12:55 +0200

    Ridotto numero di repliche (necessaria 1 gpu per il momento)

--- emotion_classification/serving/script/deleteall.sh
+++ emotion_classification/serving/script/deleteall.sh
@@ -3,3 +3,5 @@ kubectl delete -f ./emotion_classification/serving/k8/minio-credentials.yaml
 kubectl delete -f ./emotion_classification/serving/k8/minio-model.yaml
 kubectl delete -f ./emotion_classification/serving/k8/deployment.yaml
 kubectl delete -f ./emotion_classification/serving/k8/service.yaml
+kubectl delete -f ./emotion_classification/serving/k8/service-reader-role.yaml
+kubectl delete -f ./emotion_classification/serving/k8/service-reader-rolebinding.yaml
