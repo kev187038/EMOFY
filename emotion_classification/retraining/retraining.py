cc364dc1149daad2d3f755a4977cddf173f4fc6d
Placido Pellegriti <placidopellegriti@live.it>
2024-08-04 17:57:28 +0200

    Modulo retraining da integrare ad ambiente k8

--- emotion_classification/retraining/retraining.py
+++ emotion_classification/retraining/retraining.py
@@ -8,46 +8,28 @@ from tensorflow.keras.models import load_model
 from cronjob_monitor import CronJobMonitor
 from image_retriever import ImageRetriever
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 config.load_kube_config()
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from serving.utils.face_detector import FaceDetector
+#sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from face_detector import FaceDetector
+from minio import MinioClient
+import time
+
+# script periodico per aggiornamento modello
 
 
-#script periodico per aggiornamento modello
-
-
-#1) Download del modello da minio
-v1 = client.CoreV1Api()
-service = v1.read_namespaced_service(name='minio-service', namespace='default')
-minio_service = f"{service.spec.cluster_ip}:{service.spec.ports[0].port}"
-
-minio_client = Minio(
-    minio_service,  # Replace with your MinIO server URL
-    access_key=os.getenv('MINIO_ACCESS_KEY'),  # Replace with your access key
-    secret_key=os.getenv('MINIO_SECRET_KEY'),  # Replace with your secret key
-    secure=False  # Set to False if your MinIO server is not using HTTPS
-)
-
-try: minio_client.fget_object(os.getenv('MODEL_BUCKET'), os.getenv('MODEL_NAME'), 'model.h5')
-except S3Error as err: print(f"Error occurred: {err}")
-
-
-#2) Download del dataset
+# Download del dataset
 v1 = client.CoreV1Api()
 service = v1.read_namespaced_service(name='emofy-login-service', namespace='default')
 login_page_url = f"http://{service.spec.cluster_ip}:{service.spec.ports[0].port}/login"
 
-
-
 service = v1.read_namespaced_service(name='emofy-image-service', namespace='default')
 images_url = f"http://{service.spec.cluster_ip}:{service.spec.ports[0].port}/api/images/"
-
 
 login_data = {
     'username': os.getenv('LOGIN_USERNAME'),
     'password': os.getenv('LOGIN_PASSWORD'),
-    'id': "1" #TODO:CAPIRE COME OTTENERE L'ID
+    'id': "1"
 }
 
 
@@ -56,37 +38,70 @@ images = retriever.get_image_names()
 
 monitor = CronJobMonitor('default', 'retrain-cronjob')
 last_timestamp = monitor.last_execution()
-if last_timestamp: images = [image for image in images if image['timestamp'] > last_timestamp]
+if last_timestamp:
+    images = [image for image in images if image['timestamp'] > last_timestamp]
 
-#keep only those images havingthe key 'label'
+# keep only those images having the key 'label'
 images = [image for image in images if 'label' in image]
 
-print(images)
 incremental_dir = 'incremental_learning_images'
 images_path = []
 for image in images:
     folder = '{}/{}'.format(incremental_dir, image['label'])
-    if not os.path.exists(folder): os.makedirs(folder)
+    if not os.path.exists(folder):
+        os.makedirs(folder)
     path = os.path.join(folder, image['filename'])
     images_path.append(path)
-    with open(path, 'wb') as f: f.write(retriever.get_image(image['id']))
+    with open(path, 'wb') as f:
+        f.write(retriever.get_image(image['id']))
 
-
-#Now, replace each image with only the face
+# Now, replace each image with only the face
 face_detector = FaceDetector()
 for path in images_path:
     img = cv2.imread(path)
     face_img = face_detector.detect_face(img, decode=False)
-    if face_img is False: os.remove(path)
-    else: cv2.imwrite(path, face_img)
+    if face_img is False:
+        os.remove(path)
+    else:
+        cv2.imwrite(path, face_img)
 
-#3) Aggiornamento del modello
-model = load_model('model.h5') #TODO: prendi ultimo in ordine cronologico
-# Path to the new data
+# 3) Aggiornamento del modello
+minio = MinioClient()
+
+model_bucket = os.getenv('MODEL_BUCKET')
+
+models = [{'name':model.object_name, 'time':model.last_modified} for model in minio.list_files(model_bucket)]
+models.sort(key=lambda x: x['time'])
+minio.download_file('models', models[0]['name'], 'model.h5')
+model = load_model('model.h5')  # TODO: prendi ultimo in ordine cronologico
+
+# Recompile the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
 new_data_dir = 'incremental_learning_images'
+emotions = ['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise']
+# check that there is a folder inside new_data_dir for each emotion, otherwise create it
+
+# delete any folder not in emotions
+for folder in os.listdir(new_data_dir):
+    if folder not in emotions:
+        os.rmdir(folder)
+num_images = 0
+for emotion in emotions:
+    if not os.path.exists(f'{new_data_dir}/{emotion}'):
+        os.makedirs(f'{new_data_dir}/{emotion}')
+    num_images += len(os.listdir(f'{new_data_dir}/{emotion}'))
+
+if not num_images: sys.exit(0)
 
 # Create an ImageDataGenerator without data augmentation for the new data
 new_data_datagen = ImageDataGenerator(rescale=1.0/255)
+#count number of images in each folder
+for emotion in emotions:
+    num_images += len(os.listdir(f'{new_data_dir}/{emotion}'))
+
+
+# Rischio di Catastrophic Forgetting #TODO DISCUTERNE
 
 # Create a generator to read images from the new data directory
 new_data_generator = new_data_datagen.flow_from_directory(
@@ -105,4 +120,6 @@ history = model.fit(
 )
 
 # Save the updated model
-model.save('emotion_recognition_model_updated.h5')
+model_name = 'model_' + time.strftime('%Y%m%d%H%M%S') + '.h5'
+model.save(model_name)
+minio.upload_file('models', model_name, model_name)
