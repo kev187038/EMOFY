17013ddcf4bb92444d4d1342b923d49ad07e41b1
Placido Pellegriti <placidopellegriti@live.it>
2024-08-06 08:54:45 +0200

    Corretto nome immagine

--- emotion_classification/retraining/script/updatedockerimage.bat
+++ emotion_classification/retraining/script/updatedockerimage.bat
@@ -2,5 +2,5 @@
 cd emotion_classification/retraining
 docker build -t emofy-retraining:latest .
 cd ../..
-docker tag emotion_classification_retraining:latest siralex01/emofy:latest
-docker push siralex01/emofy:latest
+docker tag emotion_classification_retraining:latest siralex01/emofy-retraining:latest
+docker push siralex01/emofy-retraining:latest
