fbd5d4462ab81cc161f8c4212947d998c0f372a3
Placido Pellegriti <placidopellegriti@live.it>
2024-08-06 08:54:13 +0200

    Aggiunto logging

--- emotion_classification/retraining/retraining.py
+++ emotion_classification/retraining/retraining.py
@@ -10,21 +10,25 @@ from image_retriever import ImageRetriever
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 config.load_kube_config()
-#sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from face_detector import FaceDetector
 from minio import MinioClient
 import time
+from logger import set_logger
 
 # script periodico per aggiornamento modello
 
+logger = set_logger('retraining')
+logger.info('Starting retraining script')
 
 # Download del dataset
 v1 = client.CoreV1Api()
 service = v1.read_namespaced_service(name='emofy-login-service', namespace='default')
 login_page_url = f"http://{service.spec.cluster_ip}:{service.spec.ports[0].port}/login"
+logger.info(f'Login page URL: {login_page_url}')
 
 service = v1.read_namespaced_service(name='emofy-image-service', namespace='default')
 images_url = f"http://{service.spec.cluster_ip}:{service.spec.ports[0].port}/api/images/"
+logger.info(f'Images URL: {images_url}')
 
 login_data = {
     'username': os.getenv('LOGIN_USERNAME'),
@@ -32,17 +36,20 @@ login_data = {
     'id': "1"
 }
 
-
+logger.info('Retrieving images')
 retriever = ImageRetriever(login_page_url, images_url, login_data)
 images = retriever.get_image_names()
+logger.info(f'Total images retrieved: {len(images)}')
 
 monitor = CronJobMonitor('default', 'retrain-cronjob')
 last_timestamp = monitor.last_execution()
 if last_timestamp:
+    logger.info(f'Filtering images based on last execution timestamp: {last_timestamp}')
     images = [image for image in images if image['timestamp'] > last_timestamp]
 
-# keep only those images having the key 'label'
+# Keep only those images having the key 'label'
 images = [image for image in images if 'label' in image]
+logger.info(f'Total labeled images: {len(images)}')
 
 incremental_dir = 'incremental_learning_images'
 images_path = []
@@ -50,10 +57,12 @@ for image in images:
     folder = '{}/{}'.format(incremental_dir, image['label'])
     if not os.path.exists(folder):
         os.makedirs(folder)
+        logger.info(f'Created directory: {folder}')
     path = os.path.join(folder, image['filename'])
     images_path.append(path)
     with open(path, 'wb') as f:
         f.write(retriever.get_image(image['id']))
+        logger.info(f'Downloaded and saved image: {path}')
 
 # Now, replace each image with only the face
 face_detector = FaceDetector()
@@ -62,46 +71,59 @@ for path in images_path:
     face_img = face_detector.detect_face(img, decode=False)
     if face_img is False:
         os.remove(path)
+        logger.info(f'Removed image without detectable face: {path}')
     else:
         cv2.imwrite(path, face_img)
+        logger.info(f'Saved face image: {path}')
 
 # 3) Aggiornamento del modello
 minio = MinioClient()
-
 model_bucket = os.getenv('MODEL_BUCKET')
 
-models = [{'name':model.object_name, 'time':model.last_modified} for model in minio.list_files(model_bucket)]
+logger.info('Retrieving models from Minio')
+models = [{'name': model.object_name, 'time': model.last_modified} for model in minio.list_files(model_bucket)]
 models.sort(key=lambda x: x['time'])
-minio.download_file('models', models[0]['name'], 'model.h5')
-model = load_model('model.h5')  # TODO: prendi ultimo in ordine cronologico
+logger.info(f'Models available: {models}')
+
+if models:
+    model_name = models[-1]['name']
+    minio.download_file('models', model_name, 'model.h5')
+    logger.info(f'Downloaded model: {model_name}')
+    model = load_model('model.h5')
+    logger.info('Loaded model from file')
+else:
+    logger.error('No models found in the bucket')
+    sys.exit(1)
 
 # Recompile the model
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+logger.info('Model compiled')
 
 new_data_dir = 'incremental_learning_images'
 emotions = ['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise']
-# check that there is a folder inside new_data_dir for each emotion, otherwise create it
 
-# delete any folder not in emotions
+# Delete any folder not in emotions and create directories if they do not exist
 for folder in os.listdir(new_data_dir):
     if folder not in emotions:
-        os.rmdir(folder)
+        os.rmdir(os.path.join(new_data_dir, folder))
+        logger.info(f'Removed unused directory: {folder}')
+
 num_images = 0
 for emotion in emotions:
-    if not os.path.exists(f'{new_data_dir}/{emotion}'):
-        os.makedirs(f'{new_data_dir}/{emotion}')
-    num_images += len(os.listdir(f'{new_data_dir}/{emotion}'))
+    emotion_dir = os.path.join(new_data_dir, emotion)
+    if not os.path.exists(emotion_dir):
+        os.makedirs(emotion_dir)
+        logger.info(f'Created directory for emotion: {emotion}')
+    num_images += len(os.listdir(emotion_dir))
 
-if not num_images: sys.exit(0)
+if num_images == 0:
+    logger.info('No new images to train on. Exiting.')
+    sys.exit(0)
 
 # Create an ImageDataGenerator without data augmentation for the new data
 new_data_datagen = ImageDataGenerator(rescale=1.0/255)
-#count number of images in each folder
-for emotion in emotions:
-    num_images += len(os.listdir(f'{new_data_dir}/{emotion}'))
 
-
-# Rischio di Catastrophic Forgetting #TODO DISCUTERNE
+logger.info(f'Total number of new images: {num_images}')
 
 # Create a generator to read images from the new data directory
 new_data_generator = new_data_datagen.flow_from_directory(
@@ -112,7 +134,7 @@ new_data_generator = new_data_datagen.flow_from_directory(
     class_mode='categorical'
 )
 
-# Retrain the model on the new data
+logger.info('Starting model retraining')
 history = model.fit(
     new_data_generator,
     steps_per_epoch=new_data_generator.samples // new_data_generator.batch_size,
@@ -122,4 +144,7 @@ history = model.fit(
 # Save the updated model
 model_name = 'model_' + time.strftime('%Y%m%d%H%M%S') + '.h5'
 model.save(model_name)
+logger.info(f'Model saved as: {model_name}')
+
 minio.upload_file('models', model_name, model_name)
+logger.info(f'Uploaded model to Minio: {model_name}')
--- emotion_classification/retraining/script/createall.bat
+++ emotion_classification/retraining/script/createall.bat
@@ -2,6 +2,6 @@ call emotion_classification\script\updatedockerimage.bat
 
 copy .\emotion_classification\serving\utils\minio.py .\emotion_classification\retraining\minio.py
 copy .\emotion_classification\serving\utils\face_detector.py .\emotion_classification\retraining\face_detector.py
-
+copy .\emotion_classification\serving\utils\logger.py .\emotion_classification\retraining\logger.py
 
 kubectl create -f ./emotion_classification/retraining/k8/retrain-cron.yaml
