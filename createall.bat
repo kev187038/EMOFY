72dface4eb283636bb17aa3985343a42e4002135
Placido Pellegriti <placidopellegriti@live.it>
2024-08-16 20:33:52 +0200

    Rimossa identificazione gpu che non funziona

--- createall.bat
+++ createall.bat
@@ -1,18 +1,6 @@
-@echo off
+minikube start --driver=docker --gpus all
 
-REM Check if an NVIDIA GPU is present
-nvidia-smi >nul 2>&1
 
-REM If the previous command was successful, start minikube with GPU support
-if %ERRORLEVEL% == 0 (
-    echo NVIDIA GPU detected, starting minikube with GPU support...
-    minikube start --driver=docker --gpus all
-) else (
-    echo No NVIDIA GPU detected, starting minikube without GPU support (retraining unavailable)...
-    minikube start --driver=docker
-)
-
-REM Run the remaining scripts
 call ./login/createall.bat 
 call ./image_storage/createall.bat
 call ./image_filter/script/createall.bat
